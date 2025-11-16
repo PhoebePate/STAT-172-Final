e45662f8e56dee0a034d38c093a0b9b67c74b7e5
Phoebe Pate <2023phoebe@gmail.com>
2025-11-16 17:43:40 -0600

    Create Final Project.R

---
+++ Final Project.R
@@ -0,0 +1,16 @@
+# STAT 172 FINAL PROJECT
+
+rm(list = ls())
+library(tidyverse)
+library(reshape2)
+library(ggplot2)
+library(pROC)
+library(glmnet)
+library(forcats)
+
+games <- read.csv("class_data/boardgames.csv", stringsAsFactors = TRUE)
+
+games <- games %>% select(-c(1, 4, 10:12, 15, 17:22, 25, 30, 32, 34, 37:45, 47, 49:50, 52))
+
+
+

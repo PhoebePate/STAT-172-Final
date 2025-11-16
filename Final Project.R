0260e515c73cd828673a9ac859e8a30d02f49bbe
kaitlynstaut <kaitlyn.staut@drake.edu>
2025-11-16 17:48:44 -0600

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -12,5 +12,9 @@ games <- read.csv("class_data/boardgames.csv", stringsAsFactors = TRUE)
 
 games <- games %>% select(-c(1, 4, 10:12, 15, 17:22, 25, 30, 32, 34, 37:45, 47, 49:50, 52))
 
-
+# replace minplayers == 0 with median
+# replace maxplayers == 0 with median
+# replace minplaytime == 0 with median
+# replace maxplaytime == 0 with median
+# replace years <= 0 with median
 

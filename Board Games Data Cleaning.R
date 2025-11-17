6bf0133b77472882ed9fb9c34f878b4cd8ea2c73
kaitlynstaut <kaitlyn.staut@drake.edu>
2025-11-16 18:08:05 -0600

    Update Board Games Data Cleaning.R

--- Board Games Data Cleaning.R
+++ Board Games Data Cleaning.R
@@ -1,6 +1,8 @@
 # STAT 172 FINAL PROJECT DATA CLEANING
 
 rm(list = ls())
+
+# load packages
 library(ggplot2)
 library(tidyverse)
 library(rpart)
@@ -14,19 +16,29 @@ library(glmnet)
 library(lubridate)
 library(reshape2)
 
-
+# import data
 games <- read.csv("class_data/boardgames.csv", stringsAsFactors = TRUE)
 # our client is the Rook Room, a new small business in Des Moines who needs help in categorizing their board game collection. 
 # Their collection of games requires a difficulty indicator (whether it is a complex game or not) based on a number of factors. 
 
+# CLEANING STEPS
+# remove unwanted columns
 games <- games %>% select(-c(1, 4, 10:12, 15, 17:18, 20:22, 25, 30, 32, 34, 37:50, 52))
 
 # turn description into word count
 games$desc_word_count <- lengths(strsplit(games$description, "\\s+"))
 
 # replace minplayers == 0 with median
+
+
 # replace maxplayers == 0 with median
+
+
 # replace minplaytime == 0 with median
+
+
 # replace maxplaytime == 0 with median
+
+
 # replace years <= 0 with median
 

6b0088fb4cd9c4d425144235ed19969a5061cefa
kaitlynstaut <kaitlyn.staut@drake.edu>
2025-11-16 18:02:46 -0600

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -12,9 +12,7 @@ games <- read.csv("class_data/boardgames.csv", stringsAsFactors = TRUE)
 # our client is the Rook Room, a new small business in Des Moines who needs help in categorizing their board game collection. 
 # Their collection of games requires a difficulty indicator (whether it is a complex game or not) based on a number of factors. 
 
-
-
-games <- games %>% select(-c(1, 4, 10:12, 15, 17, 18, 20:22, 25, 30, 32, 34, 37:50, 52))
+games <- games %>% select(-c(1, 4, 10:12, 15, 17:18, 20:22, 25, 30, 32, 34, 37:50, 52))
 
 # turn description into word count
 games$desc_word_count <- lengths(strsplit(games$description, "\\s+"))

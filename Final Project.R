bf899c36e24a69e915c2bd3b675f5111bedb4fe3
erikaroehrs22-beep <erika.roehrs22@gmail.com>
2025-11-16 18:02:17 -0600

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -9,6 +9,10 @@ library(glmnet)
 library(forcats)
 
 games <- read.csv("class_data/boardgames.csv", stringsAsFactors = TRUE)
+# our client is the Rook Room, a new small business in Des Moines who needs help in categorizing their board game collection. 
+# Their collection of games requires a difficulty indicator (whether it is a complex game or not) based on a number of factors. 
+
+
 
 games <- games %>% select(-c(1, 4, 10:12, 15, 17, 18, 20:22, 25, 30, 32, 34, 37:50, 52))
 
